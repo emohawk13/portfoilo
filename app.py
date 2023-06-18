07d4a2ca1d506f65e34d3d78334642c5c3b2f8ac
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-06-18 04:14:05 -0600

    Added responsive design by adding a hamburgermnu

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def index():
         {'name': 'Contact', 'url': '/home', 'class': 'contact-button'}
     ]
     socialMenu_items = get_social_menu_items()
-    return render_template('index.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items)
+    comboMenu = mainMenu_items + socialMenu_items
+    return render_template('index.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items, comboMenu=comboMenu)
 
 
 if __name__ == '__main__':
