43fbf410ee3be3b11df6740204d8e1642c073ddd
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-06-18 03:21:36 -0600

    finished editing upper nav bar

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
 from connections import connect_db, get_social_menu_items
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-mysql = MySQL(app)
+
+# Connect to the database
+with app.app_context():
+    connect_db(app)
 
 @app.route('/')
 def index():
-    mainMenu_items = ['Home', 'About', 'Services']
+    mainMenu_items = [
+        {'name': 'Home', 'url': '/home'},
+        {'name': 'About', 'url': '/home'},
+        {'name': 'Services', 'url': '/home'},
+        {'name': 'Contact', 'url': '/home', 'class': 'contact-button'}
+    ]
     socialMenu_items = get_social_menu_items()
     return render_template('index.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items)
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        connect_db(app)  # Pass the app object as an argument to connect_db()
     app.run(debug=True)
-
-
