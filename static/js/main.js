391ee9d8f22bb967f4b8b282b475f8aa3b31bf92
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-06-18 22:03:32 -0600

    Created Contactme modal and way to add userdata

--- static/js/main.js
+++ static/js/main.js
@@ -1,52 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerIcon = document.querySelector('.hamburger-icon');
-  const menu = document.querySelector('.menu');
-
-  function handleMenuToggle() {
-    menu.classList.toggle('show-menu');
-  }
-
-  hamburgerIcon.addEventListener('click', handleMenuToggle);
-
-  // Adjust menu display based on screen size
-  const mediaQuery = window.matchMedia('(max-width: 1038px) and (max-height: 799px)');
-  const nav = document.querySelector('nav');
-  const socialBar = document.querySelector('.social');
-
-  function handleMenuDisplay(mediaQuery) {
-    if (mediaQuery.matches) {
-      menu.style.display = 'block';
-      nav.style.display = 'none';
-      socialBar.style.display = 'none';;
-    } else {
-      menu.style.display = 'none';
-      nav.style.display = 'block';
-      socialBar.style.display = 'block';
-    }
-  }
-
-  handleMenuDisplay(mediaQuery);
-
-  // Add event listener for media query changes
-  mediaQuery.addEventListener('change', function (event) {
-    handleMenuDisplay(event.target);
-  });
-
-  // Close the menu when a menu item is clicked
-  const menuItems = document.querySelectorAll('.menu li a');
-
-  menuItems.forEach(function (menuItem) {
-    menuItem.addEventListener('click', function () {
-      menu.classList.remove('show-menu');
-    });
+$(document).ready(function () {
+  // Show the modal when the contact button is clicked
+  $('.contact-button').click(function () {
+      $('#contactModal').modal('show');
   });
 });
-
-
-/*menu.style.display = 'block';
-          nav.style.display = 'none';
-          socialBar.style.display = 'none';*/
-
-/*menu.style.display = 'none';
-nav.style.display = 'block';
-socialBar.style.display = 'block';*/
