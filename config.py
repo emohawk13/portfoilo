18780cf66f708a5669afe58adc82511f55fdfdb5
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-06-29 18:35:17 -0600

    Update config.py
    adding privacy.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 SECRET_KEY = '648b858b3f3aec238f7e7c26eb824e487b52cefd8279369e'
-MYSQL_HOST = 'localhost'  # Replace with your MySQL host
-MYSQL_PORT = 3306  # Replace with your MySQL port
-MYSQL_USER = 'robitadmin'
-MYSQL_PASSWORD = 'Dcap1203+'
+MYSQL_HOST = ''  # Replace with your MySQL host
+MYSQL_PORT =   # Replace with your MySQL port
+MYSQL_USER = ''
+MYSQL_PASSWORD = ''
 MYSQL_DB = 'Portfolio'
 
 
