15e8927ba80a9cd099316011bdcebe97c58ca180
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-08-02 15:58:49 -0600

    updating to prevent cybersecurity problems

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // Show the modal when the contact button is clicked
+  $('.contact-button').click(function () {
+      $('#contactModal').modal('show');
+  });
+});
