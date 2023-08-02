8f685fdd99f81c03f6b0aa8e5a5d5fd621c1141a
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-08-02 15:58:39 -0600

    re adding git ignore

--- config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = ''
-MYSQL_HOST = ''  # Replace with your MySQL host
-MYSQL_PORT =   # Replace with your MySQL port
-MYSQL_USER = ''
-MYSQL_PASSWORD = ''
-MYSQL_DB = ''
--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  // Show the modal when the contact button is clicked
-  $('.contact-button').click(function () {
-      $('#contactModal').modal('show');
-  });
-});
