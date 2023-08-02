30292b106824a75137581e07d1040c818c19030b
Dauhson Capps <79077828+emohawk13@users.noreply.github.com>
2023-08-02 15:47:06 -0600

    updates

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-MYSQL_HOST = ''  # Replace with your MySQL host
-MYSQL_PORT =  22 # Replace with your MySQL port
-MYSQL_USER = ''
-MYSQL_PASSWORD = ''
-MYSQL_DB = 'Portfolio'
-
-
-
-
+SECRET_KEY = '648b858b3f3aec238f7e7c26eb824e487b52cefd8279369e'
+MYSQL_HOST = '0.0.0.0'  # Replace with your MySQL host
+MYSQL_PORT = 3306  # Replace with your MySQL port
+MYSQL_USER = 'emohawk13'
+MYSQL_PASSWORD = 'Dcap1203+'
+MYSQL_DB = 'port'
