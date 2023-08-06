62abd23d9f8fbddf2abbebc4d3ad971260756c18
Dauhson <emohawk13@gmail.com>
2023-08-06 21:56:06 +0000

    +New Routes and pages 4 projects

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 # app.py
 import config as config
-from flask import Flask, redirect, url_for, render_template, request
-from connections import connect_db, get_social_menu_items, get_contact_form_fields, insert_contact, generate_menu_items
+from flask import Flask, render_template, request
+from connections import main_menu, in_edu, in_project, project_items, insert_contact
 #from projectConnection import get_personal_project_items
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-# Define the function to generate contact form fields
-def generate_contact_form_fields():
-    return get_contact_form_fields()
 
 @app.route('/')
 def home():
     # Call the functions to get the menu items and contact form fields
-    mainMenu_items, socialMenu_items, comboMenu = generate_menu_items()
-    contact_form_fields = generate_contact_form_fields()
+    menu = main_menu()
+    mainMenu_items = menu['main_menu_items']
+    socialMenu_items = menu['social_menu_items']
+    comboMenu = menu['comboMenu']
+    contact_form_fields = menu['contact_form_fields']
 
     return render_template('index.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items,
                            comboMenu=comboMenu, contact_form_fields=contact_form_fields)
@@ -23,16 +23,22 @@ def home():
 @app.route('/goHome')
 def goHome():
     # Call the functions to get the menu items and contact form fields
-    mainMenu_items, socialMenu_items, comboMenu = generate_menu_items()
-    contact_form_fields = generate_contact_form_fields()
+    main_m = main_menu()
+    mainMenu_items = main_m['main_menu_items']
+    socialMenu_items = main_m['social_menu_items']
+    comboMenu = main_m['comboMenu']
+    contact_form_fields = main_m['contact_form_fields']
 
     return render_template('index.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items,
                            comboMenu=comboMenu, contact_form_fields=contact_form_fields)
 
 @app.route('/contact_submitted', methods=['POST'])
 def contact_submitted():
-    mainMenu_items, socialMenu_items, comboMenu = generate_menu_items()
-    contact_form_fields = generate_contact_form_fields()
+    main_m = main_menu()
+    mainMenu_items = main_m['main_menu_items']
+    socialMenu_items = main_m['social_menu_items']
+    comboMenu = main_m['comboMenu']
+    contact_form_fields = main_m['contact_form_fields']
     
     first_name = request.form.get('firstName')
     last_name = request.form.get('lastName')
@@ -44,11 +50,59 @@ def contact_submitted():
     except:
         return "There was an error submitting your data, please try again.", 500
 
-    return render_template('contact.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items, comboMenu=comboMenu, contact_form_fields=contact_form_fields)
+    return render_template('contact.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items, 
+                           comboMenu=comboMenu, contact_form_fields=contact_form_fields)
 
 @app.route('/projects', methods=['GET', 'POST'])
 def projects():
-    return render_template('projects.html')
+    in_project_items = in_project()
+    in_project_menu = in_project_items['project_items']
+    socialMenu_items = in_project_items['social_menu_items']
+    comboMenu = in_project_items['comboMenu']
+    contact_form_fields = in_project_items['contact_form_fields']
+    return render_template('projects.html', in_project_menu=in_project_menu, socialMenu_items=socialMenu_items, 
+                           comboMenu=comboMenu, contact_form_fields=contact_form_fields)
+
+@app.route('/edu', methods=['GET', 'POST'])
+def edu():
+    edu_menu = in_edu()
+    in_edu_menu = edu_menu['in_edu_items']
+    socialMenu_items = edu_menu['social_menu_items']
+    comboMenu = edu_menu['comboMenu']
+    contact_form_fields = edu_menu['contact_form_fields']
+    return render_template('edu.html', in_edu_menu=in_edu_menu, socialMenu_items=socialMenu_items, 
+                           comboMenu=comboMenu, contact_form_fields=contact_form_fields)
+
+@app.route('/projectBlog', methods=['GET', 'POST'])
+def projectBlog():
+    main_menu = main_menu()
+    mainMenu_items = main_menu['main_menu_items']
+    socialMenu_items = main_menu['social_menu_items']
+    comboMenu = main_menu['comboMenu']
+    contact_form_fields = main_menu['contact_form_fields']
+    return render_template('projectBlog.html', mainMenu_items=mainMenu_items, socialMenu_items=socialMenu_items, 
+                           comboMenu=comboMenu, contact_form_fields=contact_form_fields)
+
+@app.route('/eduProjects', methods=['GET', 'POST'])
+def eduProjects():
+    in_project_items = project_items()
+    in_project_menu = in_project_items['in_project']
+    socialMenu_items = in_project_items['social_menu_items']
+    comboMenu = in_project_items['comboMenu']
+    contact_form_fields = in_project_items['contact_form_fields']
+    return render_template('eduProjects.html', in_project_menu=in_project_menu, socialMenu_items=socialMenu_items, 
+                           comboMenu=comboMenu, contact_form_fields=contact_form_fields)
+    
+@app.route('/personalProjects', methods=['GET', 'POST'])
+def personalProjects():
+    in_project = project_items()
+    in_project_menu = in_project['in_project']
+    socialMenu_items = in_project['social_menu_items']
+    comboMenu = in_project['comboMenu']
+    contact_form_fields = in_project['contact_form_fields']
+    return render_template('personalProjects.html', in_project_menu=in_project_menu, socialMenu_items=socialMenu_items, 
+                           comboMenu=comboMenu, contact_form_fields=contact_form_fields)
+
 
 if __name__ == '__main__':    app.run(debug=True)
 
--- static/js/main.js
+++ static/js/main.js
@@ -23,3 +23,23 @@ $(document).ready(function () {
   });
 
 
+// Function to generate Lorem Ipsum paragraph
+function generateLoremParagraph() {
+  // Number of sentences in the paragraph (change as needed)
+  const numSentences = 4;
+
+  // Lorem Ipsum text generator
+  function generateLoremText() {
+      return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  }
+
+  let paragraph = "";
+  for (let i = 0; i < numSentences; i++) {
+      paragraph += generateLoremText() + " ";
+  }
+
+  return paragraph;
+}
+
+// Insert the generated Lorem Ipsum paragraph into the div with id "lorem-text"
+document.getElementById("lorem-text").textContent = generateLoremParagraph();
