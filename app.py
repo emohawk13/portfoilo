8bcfb165906a6847484a5b8df50c3dfbafd33d44
Dauhson <emohawk13@gmail.com>
2023-08-06 23:21:56 +0000

    fixed & verified routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import config as config
 from flask import Flask, render_template, request
-from connections import main_menu, in_edu, in_project, project_items, insert_contact
+from connections import main_menu, in_edu, in_project, insert_contact
 #from projectConnection import get_personal_project_items
 
 app = Flask(__name__)
@@ -85,8 +85,8 @@ def projectBlog():
 
 @app.route('/eduProjects', methods=['GET', 'POST'])
 def eduProjects():
-    in_project_items = project_items()
-    in_project_menu = in_project_items['in_project']
+    in_project_items = in_project()
+    in_project_menu = in_project_items['project_items']
     socialMenu_items = in_project_items['social_menu_items']
     comboMenu = in_project_items['comboMenu']
     contact_form_fields = in_project_items['contact_form_fields']
@@ -95,11 +95,11 @@ def eduProjects():
     
 @app.route('/personalProjects', methods=['GET', 'POST'])
 def personalProjects():
-    in_project = project_items()
-    in_project_menu = in_project['in_project']
-    socialMenu_items = in_project['social_menu_items']
-    comboMenu = in_project['comboMenu']
-    contact_form_fields = in_project['contact_form_fields']
+    in_project_items = in_project()
+    in_project_menu = in_project_items['project_items']
+    socialMenu_items = in_project_items['social_menu_items']
+    comboMenu = in_project_items['comboMenu']
+    contact_form_fields = in_project_items['contact_form_fields']
     return render_template('personalProjects.html', in_project_menu=in_project_menu, socialMenu_items=socialMenu_items, 
                            comboMenu=comboMenu, contact_form_fields=contact_form_fields)
 
